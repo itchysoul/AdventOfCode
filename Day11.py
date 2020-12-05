6ef45650b8b9733605454040e43729df3b4524d1
Micah Neely <mneely@clearwateranalytics.com>
2020-12-05 13:42:01 -0700

    change sums and cells hash tables to arrays, 9% improvement in runtime

--- Day11.py
+++ Day11.py
@@ -8,6 +8,8 @@ class Cell:
         self.latest_square_score = self.cell_power
 
     def power(self, grid_serial_number):
+        if self.x == 0 or self.y == 0:
+            return None
         power_level = ((self.x + 10) * self.y + grid_serial_number) * (self.x + 10)
         return int(str(int(power_level))[-3]) - 5
 
@@ -18,39 +20,44 @@ class Cell:
 class Grid:
     def __init__(self, size, grid_serial_number):
         self.size = size
-        self.cells = {(x, y): Cell(x, y, grid_serial_number).cell_power
-                           for x in range(1, size+1)
-                           for y in range(1, size+1)}
+        self.cells = [[Cell(x, y, grid_serial_number).cell_power
+                           for x in range(size+1)]
+                           for y in range(size+1)]
         self.sums = self.submatrix_sums()
 
     def submatrix_sums(self):
         # build matrix of size**2 where value of each cell is
         # sum of everything above and to left
 
+        # dummy row to make 1-indexing easier below
+        sums = [[None for _ in range(self.size + 1)]]
         # initialize first row
-        sums = {(x, 1): self.cells[(x, 1)]
-                     for x in range(1, self.size + 1)}
+        sums.append([self.cells[1][x] for x in range(self.size + 1)])
+
+        # dummy col
+        for _ in range(self.size):
+            sums.append([None])
 
         # add columns top to bottom
-        for x in range(1, self.size + 1):
-            for y in range(2, self.size + 1):
-                sums[(x, y)] = sums[(x, y - 1)] + self.cells[(x, y)]
-
+        for y in range(2, self.size + 1):
+            sums[y].extend(
+                [sums[y-1][x] + self.cells[y][x] for x in range(1, self.size + 1)]
+            )
         # add rows left to right, cells to the left will already contain sum of everything
         # up and to the left
         for x in range(2, self.size + 1):
             for y in range(1, self.size + 1):
-                sums[(x, y)] += sums[(x - 1, y)]
+                sums[y][x] += sums[y][x-1]
         return sums
 
     def calculate_square(self, anchor_x, anchor_y, square_size):
-        power = self.sums[(anchor_x + square_size - 1, anchor_y + square_size - 1)]
+        power = self.sums[anchor_y + square_size - 1][anchor_x + square_size - 1]
         if anchor_x > 1:
-            power -= self.sums[(anchor_x - 1, anchor_y + square_size - 1)]
+            power -= self.sums[anchor_y + square_size - 1][anchor_x - 1]
         if anchor_y > 1:
-            power -= self.sums[(anchor_x + square_size - 1, anchor_y - 1)]
+            power -= self.sums[anchor_y - 1][anchor_x + square_size - 1]
         if anchor_x > 1 and anchor_y > 1:
-            power += self.sums[(anchor_x - 1, anchor_y - 1)]
+            power += self.sums[anchor_y - 1][anchor_x - 1]
         return power
 
     def max_power_top_left_coordinate(self):
@@ -73,7 +80,8 @@ class Grid:
 
 grid = Grid(size=300, grid_serial_number=5177)
 print(grid.max_power_top_left_coordinate())
-
+# 1.6 seconds using hash tables 1.45 seconds using arrays ...
+ # much easier to read using hash tables
 
 #  TODO:
 # square size **2 1, 4, 9, 16
